39c7c3eec9f3f4dc265bbbc69f72f43a8e9910a3
Maximo <maxipreneste@gmail.com>
2023-12-19 02:11:48 -0300

    Implementacion de los modelos, no completo aun (19/12/23 a las 02:11)

--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class TipoCondicionDePago(models.TextChoices):
+    A_PAGAR='P','aPagar'
+    CONTADO='C','contado'
+
+class TipoComprobante (models.TextChoices):
+    FACTURA_A='A','facturaA'
+    FACTURA_B='B','facturaB'
+    FACTURA_C='C','facturaC'
+    NOTA_CREDITO='N','notaCredito'
+
+class Venta (models.Model):
+    fechaEmision=models.DateField(default=now())
+    fechaVencimiento=models.DateField()
+    importeCancelado=models.FloatField(default=0)
+    importeTotal=models.FloatField()
+    tipo=models.CharField(max_length=1,
+                          choices=TipoComprobante.choices)
+    
+class VentaDetalle(models.Model):
+    cantidad=models.IntegerField()
+    precioArticulo=models.FloatField()
+    subtotal=models.FloatField()
+
+class Articulo(models.Model):
+    codigo=models.CharField()
+    nombre=models.CharField()
+    stock=models.IntegerField()
+
+class CondicionDePagoArticulo(models.Model):
+    precio=models.FloatField()
+
+class CondicionDePago(models.Model):
+    tipo=models.CharField(max_length=1,
+                          choices=TipoCondicionDePago.choices)
+
+class Afiliado(models.Model):
+    cuit=models.IntegerField()
+    fechaAfiliacion=models.DateField()
+    nombre=models.CharField()
+
+
+
+    
