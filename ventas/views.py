002b6ecd86a30f437ef0874a0b3438b56401c11c
Maximo <maxipreneste@gmail.com>
2023-12-23 15:11:52 -0300

    intento de implementar detalles

--- ventas/views.py
+++ ventas/views.py
@@ -1,26 +1,42 @@
 from django.shortcuts import render,HttpResponse
 from .forms import VentaForm,VentaDetalleForm
 from .models import VentaDetalle
+from django.db import transaction
 # Create your views here.
+@transaction.atomic
 def alta_venta(request):
     if request.method=='GET':
         return render(request,'altaVenta.html',{
             'formVenta':VentaForm,
             'formVentaDetalle':VentaDetalleForm,
-        })      
+        }) 
+     
     if 'submit_venta' in request.POST:
         try:
             form=VentaForm(request.POST)
-            nuevaVenta=form.save(commit=False)
+            nuevaVenta=form.save(commit=False)    
             nuevaVenta.save()
             return render(request,'altaVenta.html',{
             'formVenta':VentaForm,
             'formVentaDetalle':VentaDetalleForm,
         })
-        except Exception as e:
-            #print(e) 
+        except: 
             return render(request,'altaVenta.html',{
             'formVenta':VentaForm,
             'formVentaDetalle':VentaDetalleForm,
             'error':'algo fue mal'
         })
+    if 'submit_venta_detalle' in request.POST:
+        formDetalle=VentaDetalleForm(request.POST)
+        nuevoDetalle=formDetalle.save(commit=False)
+        nuevoDetalle.precioArticulo=1
+        nuevoDetalle.subtotal=nuevoDetalle.cantidad
+        nuevoDetalle.save()
+        try:
+            pass
+        except:
+            return render(request,'altaVenta.html',{
+            'formVenta':VentaForm,
+            'formVentaDetalle':VentaDetalleForm,
+            'error':'algo fue mal en detalle'})
+
